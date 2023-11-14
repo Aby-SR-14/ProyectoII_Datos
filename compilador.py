dfe885c2ecb02bd4719b7e9220985e5172457ebd
Dilacord <122496381+Dilacord@users.noreply.github.com>
2023-11-13 21:10:21 -0600

    Update compilador.py
    text_reader()

--- compilador.py
+++ compilador.py
@@ -34,16 +34,16 @@ class Compilador:
 
         return False
 
-    def busca_elemento_hash(key):
-        for registro in tablahash_dato.items():
+    def busca_elemento_hash(self, key):
+        for registro in self.tablahash_dato.items():
             if registro[1].get_identificador() == key:
                 return registro[1]
 
-    def verifica_return(ret, nombre_funcion):
+    def verifica_return(self, ret, nombre_funcion):
         flag_ret = False
         flag_fun = False
 
-        for key in dato_key:
+        for key in self.dato_key:
             if key == ret:
                 flag_ret = True
 
@@ -51,7 +51,129 @@ class Compilador:
                 flag_fun = True
 
         if flag_fun and flag_ret:
-            if hashtable_elemento[ret].get_tipo_dato() == hashtable_elemento[nombre_funcion].get_tipo_dato():
+            if self.tablahash_dato[ret].get_tipo_dato() == self.tablahash_dato[nombre_funcion].get_tipo_dato():
                 return True
 
         return False
+    
+    def text_reader(self):
+        nombre_funcion = ""
+        palabra = ""
+        palabra2 = ""
+        contador = 0
+        bandera = 0 
+        bandera2 = False
+        contador_llaves = 0
+        compilacion_correcta = 0
+
+
+
+        nombre = ""
+        tipo = ""
+        valor = ""
+        tipo_p = ""
+        id_p = ""
+
+        with open("codigos.txt") as entrada:
+            while bandera == 0:
+                linea = entrada.readline()
+                contador += 1
+                numero_linea = contador
+
+                if not linea:
+                    break  
+
+                palabras = linea.split()
+
+                for palabra in palabras:
+                    if palabra != palabra2:
+                        if palabra == "{":
+                            contador_llaves += 1
+                        if palabra == "}":
+                            contador_llaves -= 1
+
+                        if palabra != "=":
+                            if palabra in ["int", "void", "float", "string"] and tipo == "":
+                                tipo = palabra
+                            else:
+                                if nombre == "" and palabra[0].isalpha():
+                                    nombre = palabra
+                                    if nombre[-1] == ";":
+                                        nombre = nombre[:-1] 
+
+                                    if palabra[-1] == ";":
+                                        palabra = palabra[:-1] 
+
+                                if nombre != palabra:
+                                    if nombre != "":
+                                        if nombre == "return":
+                                            palabra = palabra[:-1]
+                                            if not Dato.verifica_return(palabra, nombre_funcion):
+                                                print(
+                                                    f"Error-- la variable de retorno no es correcta conforme a la declaracion de la funcion '{nombre_funcion}'"
+                                                )
+                                                compilacion_correcta += 1
+
+                                        if tipo_p == "":
+                                            tipo_p = palabra
+                                            if tipo_p[0] == "(":
+                                                tipo_p = tipo_p[1:]
+
+                                        else:
+                                            id_p = palabra
+                                            if id_p[-1] == ")" or id_p[-1] == ",":
+                                                id_p = id_p[:-1]
+
+                                        valor = palabra
+                                        if self.es_funcion(valor):
+                                            bandera2 = True
+
+                                        if bandera2:
+                                            if id_p != "" and tipo_p != "":
+                                                funcion = Dato(tipo_p, id_p, "")
+                                                if self.valor_tipo_correcto(funcion):
+                                                    self.parametros.append(funcion)
+                                                else:
+                                                    compilacion_correcta += 1
+                                                tipo_p = ""
+                                                id_p = ""
+
+                                        else:
+                                            if valor[-1] == ";":
+                                                valor = valor[:-1]
+                                            break
+
+                                    palabra2 = palabra
+                                    bandera2 = False
+                                    palabra = ""
+                                    palabra2 = ""
+                                    tipo_p = ""
+                                    id_p = ""
+
+                if not self.parametros:
+                    ele = Dato(tipo, nombre, valor)
+                    if not self.valor_tipo_correcto(ele):
+                        print("NO compila....")
+                        compilacion_correcta += 1
+
+                else:
+                    funcion = Dato(tipo, nombre, self.parametros.copy())
+                    nombre_funcion = nombre
+                    self.dato_key.append(nombre)
+                    key = nombre
+                    registro = {key: funcion}
+                    self.tablahash_dato.update(registro)
+                    self.parametros.clear()
+
+                nombre = ""
+                tipo = ""
+                valor = ""
+
+        if contador_llaves > 0:
+            print("Error-- se esperaba '}'")
+
+        if contador_llaves < 0:
+            print("Error-- se esperaba '{'")
+
+        if compilacion_correcta == 0:
+            print("Compilacion exitosa")
